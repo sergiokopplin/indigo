06a99de1fcc9b1a5fdab7c43e17090b1fe320afa
Sergio Kopplin <sergiokopplin@gmail.com>
2016-02-08 09:18:40 -0200

    watch for _config

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('imagemin', function(tmp) {
 gulp.task('watch', function() {
     gulp.watch('src/styles/**/*', ['styles']);
     gulp.watch('src/images/**/*.{jpg,png,gif}', ['imagemin']);
-    gulp.watch(['_drafts/*','_includes/*','_layouts/*','_posts/*','*.{html,md}'], ['jekyll-rebuild']);
+    gulp.watch(['_drafts/*', '_includes/*', '_layouts/*', '_posts/*', '*.{html,md}', '_config.yml'], ['jekyll-rebuild']);
 });
 
 gulp.task('default', ['styles', 'imagemin', 'browserSync', 'watch']);
